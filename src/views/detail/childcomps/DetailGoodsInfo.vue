b29980b7de5d672233527b1a0762d461ef7ab911
big-zhangjun <1552753132@qq.com>
2021-11-23 11:51:39 +0800

    fix: 修改详情页滚动bug

--- src/views/detail/childcomps/DetailGoodsInfo.vue
+++ src/views/detail/childcomps/DetailGoodsInfo.vue
@@ -1,81 +1,81 @@
 <template>
-  <div v-if="Object.keys(detailInfo).length !== 0" class="goods-info">
-    <div class="info-desc clear-fix">
-      <div class="start"></div>
-      <div class="desc">{{ detailInfo.desc }}</div>
-      <div class="end"></div>
+  <div id="detail_goods_info" v-if="Object.keys(detailInfo).length != 0">
+    <div class="goods_info_desc">
+      <div style="float: left" class="desc_line">
+        <i class="desc_dot"></i>
+      </div>
+      <div class="desc_text">
+        {{ detailInfo.desc }}
+      </div>
+      <div style="float: right" class="desc_line">
+        <i style="right: 0px" class="desc_dot"></i>
+      </div>
     </div>
-
-    <div class="info-key">{{ detailInfo.detailImage[0].key }}</div>
-    <div class="info-list">
+    <div class="goods_info_key">
+      {{ detailInfo.detailImage[0].key }}
+    </div>
+    <div class="goods_info_imgs">
       <img
+        :src="item"
+        alt=""
         v-for="(item, index) in detailInfo.detailImage[0].list"
         :key="index"
-        :src="item"
-        @load="imgLoad"
-        alt=""
+        @load="loadImg"
       />
     </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: "DetailGoodsInfo",
   props: {
     detailInfo: {
       type: Object,
-      detault() {
+      default() {
         return {};
-      },
-    },
-  },
-  data() {
-    return {
-      counter: 0,
-      imagesLength: 0,
-    };
+      }
+    }
   },
   methods: {
-    imgLoad() {
-      //判断，所有图片加载完成，进行一个回调即可
-      //先加再对比
-      if (++this.counter === this.imagesLength) {
-        this.$emit("imageLoad");
-      }
-    },
-  },
-  watch: {
-    detailInfo() {
-      //获取图片的个数
-      this.imagesLength = this.detailInfo.detailImage[0].list.length;
-    },
-  },
+    loadImg() {
+      this.$emit("imageLoad");
+    }
+  }
 };
 </script>
-
 <style scoped>
-.goods-info {
-  padding: 20px 0;
-  border-bottom: 5px solid #f2f5f8;
+#detail_goods_info {
+  font-size: 0.68rem;
 }
-.info-desc {
+.goods_info_desc {
+  font-size: 0.6rem;
+  padding: 0.42rem 0.34rem;
 }
-/* .start {
-} */
-.desc {
-  font-size: 20px;
-  width: 100%;
-  height: 100%;
+.desc_line {
+  width: 3.5rem;
+  height: 0.04rem;
+  background-color: gray;
+  position: relative;
 }
-/* .end {
-} */
-.info-key {
-  color: pink;
+
+.desc_dot {
+  position: absolute;
+  top: -0.1rem;
+  display: inline-block;
+  width: 0.21rem;
+  height: 0.21rem;
+  background-color: black;
+  border-radius: 50%;
 }
-.info-list {
+
+.desc_text {
+  padding: 0.85rem 0;
+  color: gray;
 }
-.info-list img {
+
+.goods_info_key {
+  padding: 0.42rem 0.34rem;
+}
+.goods_info_imgs img {
   width: 100%;
 }
 </style>
