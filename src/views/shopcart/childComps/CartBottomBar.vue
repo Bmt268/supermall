3a76e3adf9cb9668b4c545bbf602bfbcc10712ac
Bmt268 <1321476266@qq.com>
2021-11-29 13:34:34 +0800

    详情页反弹问题

---
+++ src/views/shopcart/childComps/CartBottomBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bottom-bar">
+    <div class="select">
+      <check-button class="btn"></check-button>
+      <span> 全选</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import CheckButton from "components/content/checkButton/CheckButton.vue";
+export default {
+  components: { CheckButton },
+  name: "CartBottomBar",
+};
+</script>
+
+<style scoped>
+.bottom-bar {
+  background-color: #eee;
+  height: 40px;
+  position: relative;
+}
+.select {
+  display: flex;
+  align-items: center;
+}
+.btn {
+  height: 18px;
+  width: 18px;
+}
+</style>
